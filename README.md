d92b597e5a966d8284632cb84b4613e54179734e
lteu <lteu@icloud.com>
2019-09-08 18:05:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Demo AVCamera - AVFoundation
+
+This demo implements[tutorial - Creating a Custom Camera View](https://guides.codepath.com/ios/Creating-a-Custom-Camera-View) in Objective-C
--- avCamera/ViewController.m
+++ avCamera/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *previewView;
+@property (weak, nonatomic) IBOutlet UIImageView *captureImageView;
+
+@property (nonatomic) AVCaptureSession *captureSession;
+@property (nonatomic) AVCapturePhotoOutput *stillImageOutput;
+@property (nonatomic) AVCaptureVideoPreviewLayer *videoPreviewLayer;
 
 @end
 
@@ -19,5 +26,83 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    self.captureSession = [AVCaptureSession new];
+    self.captureSession.sessionPreset = AVCaptureSessionPresetPhoto;
+    
+    AVCaptureDevice *backCamera = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (!backCamera) {
+        NSLog(@"Unable to access back camera!");
+        return;
+    }
+    
+    NSError *error;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:backCamera
+                                                                        error:&error];
+    if (!error) {
+        //Step 9
+    }
+    else {
+        NSLog(@"Error Unable to initialize back camera: %@", error.localizedDescription);
+    }
+    
+    self.stillImageOutput = [AVCapturePhotoOutput new];
+    
+    
+    self.stillImageOutput = [AVCapturePhotoOutput new];
+    
+    if ([self.captureSession canAddInput:input] && [self.captureSession canAddOutput:self.stillImageOutput]) {
+        
+        [self.captureSession addInput:input];
+        [self.captureSession addOutput:self.stillImageOutput];
+        [self setupLivePreview];
+    }
+    // Setup your camera here...
+}
+
+
+- (void)setupLivePreview {
+    
+    self.videoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
+    if (self.videoPreviewLayer) {
+        
+        self.videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+        self.videoPreviewLayer.connection.videoOrientation = AVCaptureVideoOrientationPortrait;
+        [self.previewView.layer addSublayer:self.videoPreviewLayer];
+        
+        //Step12
+        dispatch_queue_t globalQueue =  dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+        dispatch_async(globalQueue, ^{
+            [self.captureSession startRunning];
+            //Step 13
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.videoPreviewLayer.frame = self.previewView.bounds;
+            });
+        });
+    }
+}
+
+- (IBAction)didTakePhoto:(id)sender {
+    AVCapturePhotoSettings *settings = [AVCapturePhotoSettings photoSettingsWithFormat:@{AVVideoCodecKey: AVVideoCodecTypeJPEG}];
+    
+    [self.stillImageOutput capturePhotoWithSettings:settings delegate:self];
+}
+
+- (void)captureOutput:(AVCapturePhotoOutput *)output didFinishProcessingPhoto:(AVCapturePhoto *)photo error:(nullable NSError *)error {
+    
+    NSData *imageData = photo.fileDataRepresentation;
+    if (imageData) {
+        UIImage *image = [UIImage imageWithData:imageData];
+        // Add the image to captureImageView here...
+        self.captureImageView.image = image;
+    }
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.captureSession stopRunning];
+}
 
 @end
